7be9284618c48cf3043d5636c6a5900beb62b54d
linstick <linstick@163.com>
2018-09-06 20:03:35 +0800

    【Improve】完善应用首页以及部分功能逻辑

---
+++ app/src/main/java/com/linstick/timeaxis/views/FeatureView.java
@@ -0,0 +1,10 @@
+package com.linstick.timeaxis.views;
+
+import android.widget.LinearLayout;
+
+/**
+ * Created by Administrator on 2018/9/6/006.
+ */
+
+public class FeatureView extends LinearLayout {
+}
